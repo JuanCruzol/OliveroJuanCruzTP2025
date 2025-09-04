3eb7425413ed69cfdf3e1c166e33bb9b1d69cd2b
Juan Cruz Olivero <124385829+JuanCruzol@users.noreply.github.com>
2025-09-04 14:15:16 -0300

    Comentarios sobre el codigo

--- TPESTRUCT/dlist.h
+++ TPESTRUCT/dlist.h
@@ -25,20 +25,43 @@ libera el dato
 */
 void destroy(void* dato);
 
+/*
+le asigno memeria a un DNodo
+*/
 DNodo* crear_nodo();
 
+/*
+Creamos una dlist
+*/
 DList* crear_list();
 
+/*
+retorna 1 si esta vacio, 0 sino
+*/
 int Dlist_vacia(DList* list);
 
+/*
+agrega un dato al inicio de la lista
+*/
 void dlist_agregar_primero(DList** list,void* dato,FuncionCopia copy);
 
+/*
+Agrego un dato al final de la lista
+*/
 void dlist_agregar_ultimo(DList** list,void* dato,FuncionCopia copy);
 
+/*
+dada una funcion aplicamos esa funcion a todos los datos de la lisat
+*/
 void dnodo_recorrer(DList* list,FuncionVisitante f);
-
+/*
+liberamos la lista y los nodos
+*/
 void destruir_Dnodo_Dlist(DList* list,FuncionDestructora destruc);
 
+/*
+hacemos una copia fisica de la lista
+*/
 DList* copia_dlist(DList*list);
 
 #endif
